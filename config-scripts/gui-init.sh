d389ae0cfd94fd32225fea81c7be28b81a79e19b
Daehwan Nam <dhnam@postech.ac.kr>
2021-12-07 23:29:59 +0900

    note update

--- config-scripts/gui-init.sh
+++ config-scripts/gui-init.sh
@@ -4,10 +4,10 @@
 # alias polybar='polybar-git'
 
 # keyboard speed
-xset r rate 250 50
+xset r rate 220 50
 
 # enable touch pad's tap-to-click
 xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1
 
 # input method
-ibus-daemon -d  # -d is daemon start optoin
+ibus-daemon -d --xim  # -d is daemon start optoin, --xim support multiple tty sessions
